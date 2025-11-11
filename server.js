82e4683306f16dcfe4d7d33f20d74586badf27b7
wbboydlll <wbboydlll@gmail.com>
2025-11-11 18:41:18 -0500

    updated the server.js to accommodate the post.js

--- server.js
+++ server.js
@@ -7,22 +7,24 @@ require('./route/users');
 const app = express(); 
 app.set('view engine', 'ejs');
 app.use('/user', userRouter);
-
-
+app.use(`/posts`, postRouter);
 app.get('/', (req, res)=>{
     console.log('Here');
-    res.render("index", {user:"George!"});
+    res.render("index", {user:"William!"});
 });
 
 app.get('/user', (req, res)=> {
     res.send('User List');
 });
-
 app.get('/users/new', (req, res)=> {
     res.send('New User Form');
 });
-
+app.get('/post', (req, res)=> {
+    res.send('Post List');
+});
+app.get('/posts/new', (req, res)=> {
+    res.send('Create New Post');
+});
 
 
 app.listen(3030);
-
