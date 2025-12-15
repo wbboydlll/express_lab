360578a3ce88b400706ff6ba45ee5fb0ad7cf0dc
wbboydlll <wbboydlll@gmail.com>
2025-12-15 17:24:43 -0500

    Updated in class

--- routes/users.js
+++ routes/users.js
@@ -3,13 +3,37 @@ require('express');
 
 const router =
 express.Router();
-
-router.get('/user', (req, res)=> {
-    res.send('User List');
+router.get('/new', (req, res)=>{
+    res.render('users/new', {firstName: "Please enter your name"});
+});
+router.get('/', (req, res)=> {
+    res.send(`user/list`, {user:users});
 });
 
-router.get('/users/new', (req, res)=> {
-    res.send('New User Form');
+router.post('/', (req, res)=>{
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+    const age = req.body.agw;
+    const gender = req.body.gender;
+    
+    
+    const isValid = firstName !=="";
+    if(isValid)    
+    {
+        console.log(`Adding User: ${firstName}`);
+        users.push({
+            firstName:firstName,
+            lastName:lastName,
+            gender:gender,
+            age:age, //trailing comma
+        });
+        console.log(`New Set of Users: ${users}`);
+        res.send("User Created!")
+    }
+    else{
+        console.log("Error adding user!");
+        res.send("user/new", {firstName:firstName});
+    }
 });
 
 module.exports = router
--- server.js
+++ server.js
@@ -1,30 +1,26 @@
-const express = 
-require('express');
+const express = require('express');
 
-const userRouter = r
-require('./route/users');
+const userRouter = require('./routes/users');
+const postRouter = require('./routes/post');
 
 const app = express(); 
 app.set('view engine', 'ejs');
-app.use('/user', userRouter);
+app.use(express.static("public"));
+app.use(express.urlencoded({extended:true}));
+//app.use(logger);
+
+app.use('/users', userRouter);
 app.use(`/posts`, postRouter);
+
 app.get('/', (req, res)=>{
     console.log('Here');
-    res.render("index", {user:"William!"});
+    res.render("index", 
+        {user:"William!"});
 });
-
-app.get('/user', (req, res)=> {
-    res.send('User List');
-});
-app.get('/users/new', (req, res)=> {
-    res.send('New User Form');
-});
-app.get('/post', (req, res)=> {
-    res.send('Post List');
-});
-app.get('/posts/new', (req, res)=> {
-    res.send('Create New Post');
-});
-
 
 app.listen(3030);
+
+function logger (req, res, next){
+    console.log(`Page Accessed: ${req.originalUrl}`);
+    next();
+}
